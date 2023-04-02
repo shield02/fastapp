a3d3126f493d855c2d1844085377ab57d5915370
Otuekong <myshield02@gmail.com>
2023-04-02 22:52:39 +0100

    Included auth routers line

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Depends, Header, HTTPException
-from routers import users, articles
-
-User = users.User
+from routers import auth, users, articles
 
 
 app = FastAPI()
@@ -11,24 +9,12 @@ app = FastAPI()
 # include other routers
 app.include_router(users.router)
 app.include_router(articles.router)
+app.include_router(auth.login.router)
+
 
 async def verify_token(x_token: Annotated[str, Header()]):
     if x_token != "fake-super-secret-token":
         raise HTTPException(status_code=400, detail="X-Token header invalid")
-
-
-
-
-
-async def get_current_user(token: Annotated[str, Depends()]):
-    user = decode_token(token)
-    return user
-
-
-# get authenticated current user
-@app.get("/me")
-def get_current_auth_user(auth_user: Annotated[User, Depends(get_current_user)]):
-    return auth_user.dict()
 
 
 
